47dfc8a7afdeefc2b2701a03dd9d093fd19a423d
Artemij Rilev <artik200050@gmail.com>
2024-04-23 18:52:44 +0300

    add last_executed_transactions_dates function

--- func.py
+++ func.py
@@ -9,3 +9,15 @@ def load_executed_transactions_list():
             executed_transactions_list.append(transaction)
 
     return executed_transactions_list
+
+
+def last_executed_transactions_dates(transactions_count=5):
+    last_transactions_dates = []
+    for transaction in load_executed_transactions_list():
+        date = transaction.get("date")
+        last_transactions_dates.append(date)
+    last_transactions_dates.sort(reverse=True)
+
+    return last_transactions_dates[0:transactions_count+1]
+
+
