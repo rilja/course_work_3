1d24e40234d6850007af33ee7d63508dd856cc52
Artemij Rilev <artik200050@gmail.com>
2024-04-25 16:14:41 +0300

    add 'from_account_info' abstraction to class 'Transaction'

--- transaction.py
+++ transaction.py
@@ -15,3 +15,27 @@ class Transaction:
     def transaction_description(self):
         transaction_description = self.full_transaction_info.get("description")
         return transaction_description
+
+    def from_account_info(self):
+        from_account_info = self.full_transaction_info.get("from")
+        if from_account_info is None:
+            return f''
+        txt_in_info = from_account_info.split()
+        card_name = []
+        for txt in txt_in_info:
+            if txt.isdigit():
+                card_number = txt
+            else:
+                card_name.append(txt)
+        secret_card_number = list(card_number)
+        for num in range(6, 12):
+            secret_card_number[num] = "*"
+        for num in range(4, 16, 5):
+            secret_card_number.insert(num, " ")
+        secret_card_number = ''.join(secret_card_number)
+        full_card_name = ' '.join(card_name)
+        new_from_account_info = full_card_name + " " + secret_card_number
+
+        return new_from_account_info
+
+
