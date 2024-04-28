5f03670a8cd05fe48b16d78ee90e2b91408691e1
Artemij Rilev <artik200050@gmail.com>
2024-04-28 15:37:44 +0300

    add test to last_executed_transactions_dates func and full_transactions_from_date func

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,6 +1,27 @@
 from course_work_3.src.func import *
-
+import time
 
 def test_load_executed_transactions_list():
     for transaction in load_executed_transactions_list():
         assert transaction.get("state") == "EXECUTED"
+
+
+def test_last_executed_transactions_dates():
+    last_date = last_executed_transactions_dates(1)
+    new_last_date = time.strptime(last_date[0], "%Y-%m-%dT%H:%M:%S.%f")
+    for transaction in load_executed_transactions_list():
+        transaction_date = transaction.get('date')
+        new_transaction_date = time.strptime(transaction_date, "%Y-%m-%dT%H:%M:%S.%f")
+        if new_transaction_date > new_last_date:
+            newest_date = new_transaction_date
+            break
+        else:
+            newest_date = new_last_date
+
+    assert newest_date == new_last_date
+
+
+def test_full_transactions_from_date():
+    assert full_transactions_from_date("2019-08-26T10:50:58.294041") == load_executed_transactions_list()[0]
+    assert full_transactions_from_date("2019-07-03T18:35:29.512364") == load_executed_transactions_list()[1]
+    assert full_transactions_from_date("2018-06-30T02:08:58.425572") == load_executed_transactions_list()[2]
